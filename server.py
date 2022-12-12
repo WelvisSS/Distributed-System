7c42fbd6e7f53ec19baf903cfa5e50d2045fdaaa
Welvis <welvisconta@gmail.com>
2022-12-11 21:10:55 -0300

    update

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ class Server():
         self.host = host
         self.port = port
         self.mensagensRecebidas = 0
-        self.numConexoes = 1
+        self.numConexoes = 3
         self.clients = []
         self.numOcorrencias = 0
 
