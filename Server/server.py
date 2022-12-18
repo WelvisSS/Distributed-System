65763e658ed641b4cbb016ae5a202276f8df59d4
Welvis <welvisconta@gmail.com>
2022-12-18 14:17:22 -0300

    v2

--- Server/server.py
+++ Server/server.py
@@ -8,37 +8,55 @@ from zipfile import ZipFile
 
 absolute_path = os.path.dirname(__file__)
 
-class Client():
+class Server():
     def __init__(self, host='localhost', port=7777):
         super().__init__()
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sockname = None
-        self.clientName = None
-        self.file_name = None
-        self.file_size = None
-        self.keyword = None
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = host
         self.port = port
+        self.mensagensRecebidas = 0
+        self.numConexoes = 3
+        self.clients = []
+        self.numOcorrencias = 0
+        self.file_index = None
+        self.keyword = None
+        self.client = None
+        self.file_name = None
+        self.file_size = None
         self.portSize = 2048
-        self.file_index = 0
 
     def main(self):
         try:
-            self.client.connect((self.host, self.port))
-            print('\nCliente Conectado!')
+            # Iniciando o servidor no host e porta especificada
+            self.server.bind((self.host, self.port))
+            # Servidor ouvindo
+            self.server.listen()
+            print(f'Servidor rodando em {self.host} na porta {self.port}\n')
         except:
-            return print('\nNão foi possível se conectar ao servidor!\n')
+            return print('\nNão foi possível iniciar o servidor!\n')
 
-        self.clientName = input('Usuário> ')
+        # Laço que se repete a cada conexão estabelecida
+        while True:
+            # Recebendo conexões de clientes
+            client, addr = self.server.accept()
+            # Adiciona cada novo cliente na lista de clientes
+            self.clients.append(client)
 
-        thread1 = threading.Thread(target=self.receiveMessages, args=[])
+            self.client = client
+            # Cria uma thread para cada conexão estabelacida
+            # thread = threading.Thread(target=self.messagesTreatment, args=[client])
+            thread = threading.Thread(target=self.receiveMessages, args=[client])
+            # Inicia a thread
+            thread.start()
 
-        thread1.start()
-    
-    def receiveMessages(self):
+            # Quando o número de conexões definido é alcançado, inicia o processo de envio dos arquivos
+            print('Conectado!\n')
+
+
+    def receiveMessages(self, client):
         while True:
 
-            msg = self.client.recv(self.portSize).decode().split(' ')
+            msg = client.recv(self.portSize).decode().split(' ')
             
             self.file_index = int(msg[0])
             self.file_name = f'recebido{self.file_index}.zip'
@@ -61,16 +79,16 @@ class Client():
             file = open(self.file_name, "wb")        
 
             for i in range(total_packages):
-                data = self.client.recv(self.portSize)
+                data = client.recv(self.portSize)
                 file_bytes += data
 
             file.write(file_bytes)
             file.close()
             print('Preparando o envio do arquivo\n')
-            self.sendMessages()
+            self.sendMessages(client)
 
-    def sendMessages(self):
-
+    def sendMessages(self, client):
+        
         total = self.fileExtract()
 
         file_name = f'txt{self.file_index}.txt'
@@ -86,9 +104,9 @@ class Client():
         print('mensagem enviada')
 
         file_size = os.path.getsize(file_name) 
-        self.client.send(file_name.encode())
-        self.client.send(str(file_size).encode())
-        self.client.sendall(data)
+        client.send(file_name.encode())
+        client.send(str(file_size).encode())
+        client.sendall(data)
 
         # Apagando o arquivo txt com o resultado
         if os.path.exists(file_name): 
@@ -118,4 +136,7 @@ class Client():
 
         return result
 
-Client().main()
+        
+
+p = input('Informe a porta para este servidor:\n')
+Server(port=int(p)).main()
